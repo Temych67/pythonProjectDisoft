d6769767d959793faa9234f7112cc6920ed13acd
Artem <voloshko9361@gmail.com>
2022-02-08 19:21:04 +0200

    delete line

--- testtaskDisoftproject/account/api/views.py
+++ testtaskDisoftproject/account/api/views.py
@@ -1,6 +1,5 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
 from account.api.serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token
 
